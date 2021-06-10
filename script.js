c2c362e9e1dde4d4daca1fe7a42a23f819f7af53
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-10 12:53:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,12 @@ player.addEventListener('play', () => {
   setInterval(async () => {
     const ctx = canvas.getContext('2d');
     ctx.drawImage(player, 0, 0);
-    const pose = await net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride).catch((e) => {txt.textContent = e})
+    //const pose = await net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride).catch((e) => {txt.textContent = e})
+    await net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride)
+    then((pose) => {
+      txt.textContent = "pose2"
+    })
+    .catch((e) => {txt.textContent = e})
     txt.textContent = "pose"//JSON.stringify(pose)
     
     //drawParts(ctx, pose);
